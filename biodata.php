4bdba6eb4410e52f9fee73ec275bd77f78ebd8e8
chanmelo <159239434+chanmelo@users.noreply.github.com>
2024-03-06 15:10:48 +0700

    Update biodata.php
    update biodata chantika

--- biodata.php
+++ biodata.php
@@ -1,3 +1,12 @@
 <html>
-  
+  <head>
+    <title>Biodata</title>
+  </head>
+  <body>
+    <?php
+      echo "Nama: Chantika Melolita";
+      echo "Kelas: 22 MI";
+      echo "NIM: 22110049";
+    <?
+  </body>
 </html>
